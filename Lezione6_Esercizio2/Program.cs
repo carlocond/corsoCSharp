20ef57f2ac1c34e134d561794057446e106ee310
carlocond <carlocondello05@gmail.com>
2025-05-19 17:24:15 +0200

    Add files via upload

---
+++ Lezione6_Esercizio2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Persona
+{
+    public string Nome;
+    public string Cognome;
+    public int AnnoDiNascita;
+
+
+
+    public Persona(string nome, string cognome, int anno) {
+        Nome = nome;
+        Cognome = cognome;
+        AnnoDiNascita = anno;
+
+    }
+    public void StampaDati()
+    {
+        Console.WriteLine($"Nome: {Nome} \n Cognome: {Cognome} \n Anno di nascita: {AnnoDiNascita}");
+    }
+
+    public static void Main(string[] args)
+    {
+        string nome = "";
+        string cognome = "";
+        int anno = 0;
+        int pers = 1;
+
+        while (pers <= 2)
+        {
+
+            Console.WriteLine("Inserisci il nome della persona");
+            nome = Console.ReadLine();
+            Console.WriteLine("Inserisci il cognome della persona");
+            cognome = Console.ReadLine();
+            Console.WriteLine("Inserisci l'anno di nascita");
+            anno = int.Parse(Console.ReadLine());
+
+            if (pers == 1)
+            {
+                Persona persona1 = new Persona(nome, cognome, anno);
+                persona1.StampaDati();
+            }
+            else
+            {
+                Persona persona2 = new Persona(nome, cognome, anno);
+                persona2.StampaDati();
+            }
+            pers++;
+
+
+        }
+        
+
+        
+    }
+}
