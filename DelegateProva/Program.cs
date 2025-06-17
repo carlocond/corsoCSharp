5d01d4fe42c42e9f032e2bd049248bcc224d8ada
carlocond <carlocondello05@gmail.com>
2025-06-17 16:59:10 +0200

    Add files via upload

---
+++ DelegateProva/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+delegate void Saluto(string nome);
+
+class Program
+{
+    static void Main()
+    {
+        Saluto s = Ciao;
+        s("Carlo");
+
+        Saluto a = Addio;
+        a("Carlo");
+    }
+
+    static void Ciao(string nome)
+    {
+        Console.WriteLine($"Ciao, {nome}!");
+    }
+
+    static void Addio(string nome)
+    {
+        Console.WriteLine($"Addio, {nome}!");
+    }
+}
